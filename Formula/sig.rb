e971631ffc1a1b5b84e625371f41f84592f7bfd9
zhirong <hanzhirong@kuaishou.com>
2020-05-08 10:39:00 +0800

    u

--- Formula/sig.rb
+++ Formula/sig.rb
@@ -2,6 +2,7 @@ class Sig < Formula
   desc "A Simple CLI"
   homepage "https://github.com/melotusme/sig"
   url "git@github.com:melotusme/sig.git", :using => :git
+  version "0.0.1"
 
   def install
     # 安装源代码
