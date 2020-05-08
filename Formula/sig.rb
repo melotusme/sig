880f4e18171199383b4069314de17cc5632e887b
zhirong <hanzhirong@kuaishou.com>
2020-05-08 10:37:39 +0800

    u

--- Formula/sig.rb
+++ Formula/sig.rb
@@ -1,18 +1,10 @@
 class Sig < Formula
   desc "A Simple CLI"
-  homepage "https://github.com/X140Yu/homebrew-foo/blob/master/foo"
-  url "", :using => :git
+  homepage "https://github.com/melotusme/sig"
+  url "git@github.com:melotusme/sig.git", :using => :git
 
   def install
     # 安装源代码
-    libexec.install Dir["bin/*"]
-    # 创建 foo 命令
-    bin.write_exec_script (libexec/"kws_sig")
-
-    foo_path = (bin/"foo")
-
-    # 修改 foo 的权限，让它可以被更改
-    FileUtils.chmod 0755, foo_path
-
+    bin.install Dir["bin/*"]
   end
 end
